42e97ff44c23d3591eb5977778612a00b17120b2
reamedzo@st.ug.edu.gh <reamedzo@st.ug.edu.gh>
2023-07-17 05:59:30 +0000

    Updating the project

---
+++ index.py
@@ -0,0 +1,25 @@
+num1 = Element("num1")
+num2 = Element("num2")
+operator = Element("cal")
+screen = Element("opera")
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b 
+
+def multi(a,b):
+    return a*b
+
+def divid(a,b):
+    return a/b 
+
+
+
+
+def answer(*a,**ab):
+    # if operator == "+":
+    #     result = add(num1.value,num2.value)
+    screen.write("num1")
+    
