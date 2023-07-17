5788bfe4ab2d67c6ee8feedbe3a7a4138159c549
Edem Robin <robinedemamedzo@gmail.com>
2023-07-17 10:07:10 +0000

    Update

--- index.py
+++ index.py
@@ -1,25 +1,42 @@
 num1 = Element("num1")
 num2 = Element("num2")
-operator = Element("cal")
-screen = Element("opera")
+oper = Element("opera")
+screen = Element("ans")
 
 def add(a,b):
     return a+b
-
 def sub(a,b):
     return a-b 
+def mul(a,b):
+    return a*b 
+def div(a,b):
+    return a/b
 
-def multi(a,b):
-    return a*b
-
-def divid(a,b):
-    return a/b 
-
-
-
+def clear():
+    num1.clear()
+    num2.clear()
+    oper.clear()
 
 def answer(*a,**ab):
-    # if operator == "+":
-    #     result = add(num1.value,num2.value)
-    screen.write("num1")
+    result = f"Num1={num1.value} Num2={num2.vlaue} Oper={oper.value}"
+    screen.write(result)
+ 
+    
+    # if oper.value == "+":
+    #     result = add(int(num1.value),int(num2.value))
+    #     screen.write(result)
+    # elif oper.value =="-":
+    #     result = sub(int(num1.value),int(num2.value))
+    #     screen.write(result)
+
+    # elif oper.value =="x" or oper.value =="*":
+    #     result = mul(int(num1.value),int(num2.value))
+    #     screen.write(result)
+    # elif oper.value =="/":
+    #      result = div(int(num1.value),int(num2.value))
+    #      screen.write(result)
+    # else:
+    #     screen.write(0)
+    #     clear()
+
     
